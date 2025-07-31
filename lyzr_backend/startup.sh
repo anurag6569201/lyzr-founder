3a5e270025a4bf22a0852499c6a06e8ad928e77d
anurag6569201@gmail.com <140162738+anurag6569201@users.noreply.github.com>
2025-07-31 19:12:07 +0530

    updated startup script

--- lyzr_backend/startup.sh
+++ lyzr_backend/startup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 echo "Applying database migrations..."
 python manage.py migrate --noinput
-echo "Starting Daphne server..."
-daphne -b 0.0.0.0 -p 8000 lyzr_backend.asgi:application &
 
 echo "Starting Celery worker..."
 celery -A lyzr_backend.celery worker -l info --pool=threads &
 
-wait
+echo "Starting Daphne server (in foreground)..."
+exec daphne -b 0.0.0.0 -p 8000 lyzr_backend.asgi:application
