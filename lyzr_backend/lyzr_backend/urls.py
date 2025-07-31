7aae671fc83d52d1949f38ac228f98380719e115
anurag6569201@gmail.com <140162738+anurag6569201@users.noreply.github.com>
2025-08-01 02:10:59 +0530

    updated health api

--- lyzr_backend/lyzr_backend/urls.py
+++ lyzr_backend/lyzr_backend/urls.py
@@ -4,10 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.http import JsonResponse
 
+health_check_view = lambda r: JsonResponse({"status": "ok"})
+
 urlpatterns = [
+    path('', health_check_view), 
+    path('health/', health_check_view),  
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls')),
-    path("health/", lambda r: JsonResponse({"status": "ok"})),
 ]
 
 if settings.DEBUG:
